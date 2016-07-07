7b74ce8ad85450fc3be3d22f9cde8663c0bc9047
pyama86 <pyama@pepabo.com>
2016-07-08 01:04:38 +0900

    to 0.0.3

--- pinc.rb
+++ pinc.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
-HOMEBREW_PINC_PASSWD_VERSION='0.0.2'
+HOMEBREW_PINC_PASSWD_VERSION='0.0.3'
 class Pinc < Formula
   homepage 'https://github.com/pyama86/pinc'
   url "https://github.com/pyama86/pinc/releases/download/v#{HOMEBREW_PINC_PASSWD_VERSION}/pinc_darwin_amd64.zip"
-  sha256 "24fd95197e7b460049db37d195e3e7d68146c7a99f43cdb0eee913856112c60d"
+  sha256 "a08e21abc75296810da15c182d03c60b7b342e0f989a3d7f7339a584d4c5a43a"
   version HOMEBREW_PINC_PASSWD_VERSION
   head 'https://github.com/pyama86/pinc.git', :branch => 'master'
 
